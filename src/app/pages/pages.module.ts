6f2f9d1a88b13896d28ab68f118088b0c82f4140
Ali <ali.rivera@thalamuscorp.com>
2018-09-30 00:53:04 -0300

    Sección 8 terminada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,7 +5,7 @@ import { Graficas1Component } from "./graficas1/graficas1.component";
 import { PagesComponent } from "./pages.component";
 
 import { ChartsModule } from "ng2-charts";
-
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from "@angular/forms";
 
 import { SharedModule } from "../shared/shared.module";
@@ -13,6 +13,8 @@ import { PAGES_ROUTES } from "./pages.routes";
 import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
 import { GraficoDonaComponent } from "../components/grafico-dona/grafico-dona.component";
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
     declarations: [
@@ -22,7 +24,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         PagesComponent,
         IncrementadorComponent,
         GraficoDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent,
     ],
     exports: [
         DashboardComponent,
