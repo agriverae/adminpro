ede8936458587033a86035d7a14fc5c2023c4982
Ali <ali.rivera@thalamuscorp.com>
2018-10-28 22:58:54 -0300

    Seccion 13 terminada

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { UsuarioService } from '../services/service.index';
 import { Usuario } from '../models/usuario.model';
 
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -15,16 +16,40 @@ export class LoginComponent implements OnInit {
 
   recuerdame: boolean = false;
   email: string;
+  auth2: any;
 
   constructor( public router: Router, public _usuarioService: UsuarioService) { }
 
   ngOnInit() {
     init_plugins();
 
+    this.googleInit();
     this.email = localStorage.getItem('email') || '';
     if(this.email.length > 1) {
       this.recuerdame = true;
     }
+  }
+
+  googleInit(){
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '170223500199-fbrcov6d5atmh6dgepcn73em48647qlu.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email',        
+      });
+
+      this.attachSignIn(document.getElementById('btnGoogle'));
+    });
+  }
+
+  attachSignIn( element ){
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+      let token = googleUser.getAuthResponse().id_token;
+      console.log(token);
+      this._usuarioService.loginGoogle(token).subscribe( () => {
+        window.location.href = '#/dashboard';
+      })
+    })
   }
 
   ingresar(forma: NgForm){
